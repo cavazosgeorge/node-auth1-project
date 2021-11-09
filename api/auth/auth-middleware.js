7eb83502cd75493e12943c812ac4b166258d8d4d
George Cavazos <cavazosgeorge@yahoo.com>
2021-11-09 17:49:20 -0500

    Auth Middleware: checkPasswordLength function created

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -80,8 +80,28 @@ async function checkUsernameExists(req, res, next) {
     "message": "Password must be longer than 3 chars"
   }
 */
-function checkPasswordLength() {}
+function checkPasswordLength(req, res, next) {
+  const { password } = req.body;
+
+  if (
+    !password ||
+    (typeof password === "string" && password.trim().length < 4)
+  ) {
+    next({
+      sucess: false,
+      status: 422,
+      message: "Password must be longer than c chars",
+    });
+  } else {
+    next();
+  }
+}
 
 // Don't forget to add these to the `exports` object so they can be required in other modules
 
-module.exports = { restricted, checkUsernameFree, checkUsernameExists };
+module.exports = {
+  restricted,
+  checkUsernameFree,
+  checkUsernameExists,
+  checkPasswordLength,
+};
