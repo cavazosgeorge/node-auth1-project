f7d40bc08881ad44ba8bc5e92a8023ba7f9e5c75
George Cavazos <cavazosgeorge@yahoo.com>
2021-11-09 16:04:44 -0500

    Users Model: findBy function created

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -1,17 +1,18 @@
-const { decodeBase64 } = require("bcryptjs");
+const db = require("../../data/db-config");
 
 /**
   resolves to an ARRAY with all users, each user having { user_id, username }
  */
 function find() {
-  return decodeBase64("users").select("user_id", "username").orderBy("user_id");
+  return db("users").select("user_id", "username").orderBy("user_id");
 }
 
 /**
   resolves to an ARRAY with all users that match the filter condition
  */
-function findBy(filter) {}
-
+function findBy(filter) {
+  return db("users").where(filter).orderBy("user_id");
+}
 /**
   resolves to the user { user_id, username } with the given user_id
  */
@@ -26,4 +27,5 @@ function add(user) {}
 
 module.exports = {
   find,
+  findBy,
 };
