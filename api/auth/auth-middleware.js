58ede7f07afe0ee0b51cf302f1e46542188e90b8
George Cavazos <cavazosgeorge@yahoo.com>
2021-11-09 17:29:43 -0500

    Auth Middleware: restrictedted function created

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,3 +1,4 @@
+const Users = require("../users/users-model");
 /*
   If the user does not have a session saved in the server
 
@@ -6,8 +7,16 @@
     "message": "You shall not pass!"
   }
 */
-function restricted() {
-
+function restricted(req, res, next) {
+  if (req.session.user) {
+    next();
+  } else {
+    next({
+      sucess: false,
+      status: 401,
+      message: "You shall not pass!",
+    });
+  }
 }
 
 /*
@@ -18,9 +27,6 @@ function restricted() {
     "message": "Username taken"
   }
 */
-function checkUsernameFree() {
-
-}
 
 /*
   If the username in req.body does NOT exist in the database
@@ -30,9 +36,7 @@ function checkUsernameFree() {
     "message": "Invalid credentials"
   }
 */
-function checkUsernameExists() {
-
-}
+function checkUsernameExists() {}
 
 /*
   If password is missing from req.body, or if it's 3 chars or shorter
@@ -42,8 +46,8 @@ function checkUsernameExists() {
     "message": "Password must be longer than 3 chars"
   }
 */
-function checkPasswordLength() {
-
-}
+function checkPasswordLength() {}
 
 // Don't forget to add these to the `exports` object so they can be required in other modules
+
+module.exports = { restricted, checkUsernameFree };
